4bd05d620320ed82367b7f52c4fc0f68b4e308d1
SANDY281093 <santhos281093@gmail.com>
2022-11-21 23:39:04 +0530

    second

--- src/com/sample/Sample.java
+++ src/com/sample/Sample.java
@@ -3,9 +3,15 @@ package com.sample;
 public class Sample {
 	public void task1() {
 		System.out.println("King");
+	}	
+	public void task() {
+		System.out.println("ok");
 	}
+	
+	
 public static void main(String[] args) {
 	Sample s = new Sample();
 	s.task1();
+	s.task();
 }
 }
